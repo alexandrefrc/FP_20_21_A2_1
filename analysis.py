e3a3f83fd31cd994f26122950338e6db04543596
martaspbat <68147432+martaspbat@users.noreply.github.com>
2020-12-31 16:29:08 +0000

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,1 +1,51 @@
+import pandas as pd
+
+from extract_scores import Scores as sc
+
+#Functions
+
+athletes = pd.read_csv('athletes.csv')
+
+sports_events = pd.read_csv('sports_events.csv')
+
+sports_events['qualified'] = sports_events['qualified'].astype(bool)
+
+sports_events = sports_events.dropna()
+
+sports_events['lift_valid'] = sports_events['lift_valid'].astype(bool)
+
+#sports_events = sc(sports_events)
+
+#print(sports_events.qualified_athletes())
+
+#print(sports_events.valid_lifts())
+
+
+df_grouped = sports_events.groupby(['year','athlete_id','lift_type'])
+
+results = pd.DataFrame(columns = sports_events.columns)
+
+for key, group in df_grouped:
+    group = sc(group)
+    results = results.append(group.best_lift())
+    
+results_grouped = results.groupby(['year','athlete_id'])
+
+final_score_dict = { }
+    
+for key, group in results_grouped:
+    year = group.iloc[0,0]
+    athlete_id = group.iloc[0,1]
+    cj = float(group.iloc[0,-2])
+    snatch = float(group.iloc[1,-2])
+    final_score = cj + snatch
+    final_score_dict[athlete_id] = [year,snatch,cj,final_score]
+    
+
+final_score_df = pd.DataFrame.from_dict(final_score_dict, orient='index',
+                                        columns=['Year', 'Snatch',
+                                             'Clean and Jerk',
+                                             'Final Score'])
+print(final_score_df.sort_values(by=['Year','Final Score'], ascending=False))
+
 
--- extract_scores.py
+++ extract_scores.py
@@ -1,1 +1,24 @@
+import pandas as pd
+
+class Scores:
+    
+    def __init__(self, df):
+        
+        self.df   = df
+        
+    def qualified_athletes(self):
+        return self.df[self.df.qualified]
+    
+    def valid_lifts(self):
+        self.df = self.qualified_athletes()
+        return self.df[self.df.lift_valid]
+    
+    def best_lift(self):
+        self.df = self.valid_lifts()
+        if self.df.empty:
+            return
+        else:
+            return self.df.loc[ self.df['lift'].idxmax() ]
+        
+        
 
