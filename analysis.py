bb95cc3d82a4076fa2284049a7c0d9eecacb889e
Alexandre <alexfrcoelho@gmail.com>
2021-01-15 00:47:03 +0000

    Created prints for better understanding.

--- analysis.py
+++ analysis.py
@@ -76,8 +76,8 @@ complete_info_df = complete_info_df.reset_index()
 
 complete_info_df = complete_info_df.sort_values(by='Score', ascending=False)
 
+# TABLE 1
 # Tabela razões entre peso levantado e peso do atleta
-# Complementar com médias etc.
 
 position_by_ratio_df = pd.DataFrame()
 
@@ -85,28 +85,32 @@ for index, year_results in complete_info_df.groupby('Year'):
     year_results = year_results.copy()
     year_results['Snatch/Weight'] = year_results.apply(lambda x: calc_ratio(x['Snatch'], x['Bodyweight']), axis = 1)
     year_results['Clean&Jerk/Weight'] = year_results.apply(lambda x: calc_ratio(x['Clean and Jerk'], x['Bodyweight']), axis = 1)
-    year_results['Average_Ratio'] = (year_results['Snatch/Weight']+year_results['Clean&Jerk/Weight'])/2
+    year_results['Average Ratio'] = (year_results['Snatch/Weight']+year_results['Clean&Jerk/Weight'])/2
     year_results = year_results.sort_values(
-        by='Average_Ratio', ascending=False)
+        by='Average Ratio', ascending=False)
     year_results['Position'] = year_results['Score'].rank(ascending=False, method='first')
-    year_results['Position_by_ratio'] = year_results['Average_Ratio'].rank(ascending=False, method='first')
+    year_results['Position by ratio'] = year_results['Average Ratio'].rank(ascending=False, method='first')
     year_results = year_results.drop(columns=['Nation'])
     position_by_ratio_df = position_by_ratio_df.append(year_results.head())
     
 pd.set_option('display.max_columns', 12)
 
+print('Weight lifted vs. Bodyweight ratio.\nNew positions are based on the average ratio of Snatch and Clean and Jerk:\n')
 print(position_by_ratio_df)
+print('All time highest average ratio: {}'.format(round(position_by_ratio_df['Average Ratio'].max(), 3)))
+print('Average Ratio: {}'.format(round(position_by_ratio_df['Average Ratio'].mean(), 3)))
+print('Standard Deviation: {}\n'.format(round(position_by_ratio_df['Average Ratio'].std(), 5)))
 
-# Plot médias atletas e médias medalhistas
-# Rever sintax e melhorar aparência
+# GRAPH 1
+# Gráfico médias atletas e médias medalhistas
 
-ax = complete_info_df.groupby('Year').Score.mean().plot(xticks=complete_info_df['Year'].unique(), style='o-')
+avg_all_athletes = complete_info_df.groupby('Year').Score.mean().plot(xticks=complete_info_df['Year'].unique(), style='o-')
 
 medalists = complete_info_df.groupby('Year').head(n=3)
 medalists.groupby('Year')['Score'].mean().plot(
-    xticks=complete_info_df['Year'].unique(), ax=ax, style='o-')
-ax.legend(['All Athletes', 'Medalists'], loc = 'upper center')
-ax.set_ylabel('Score (kg)')
+    xticks=complete_info_df['Year'].unique(), ax=avg_all_athletes, style='o-')
+avg_all_athletes.legend(['All Athletes', 'Medalists'], loc = 'upper center')
+avg_all_athletes.set_ylabel('Score (kg)')
 plt.title('Average Score per Year')
 plt.ylim(215, 285)
 plt.margins(x = 0.1)
@@ -116,42 +120,52 @@ ys_athletes = complete_info_df.groupby('Year')['Score'].mean()
 xs_medalists = np.sort(medalists['Year'].unique())
 ys_medalists = medalists.groupby('Year')['Score'].mean()
 
-
 for pair in [(xs_athletes, ys_athletes),(xs_medalists, ys_medalists)]:
     for x, y in zip(pair[0], pair[1]):
         label = "{:.2f}".format(y)
-        plt.annotate(label,  # this is the text
-                     (x, y),  # this is the point to label
-                     textcoords="offset points",  # how to position the text
-                     xytext=(0, 10),  # distance from text to points (x,y)
-                     bbox=dict(boxstyle="round", fc="0.9", ec="gray"),
-                     ha='center')  # horizontal alignment can be left, right or center
-
+        plt.annotate(label, (x, y), textcoords="offset points", xytext=(0, 10), bbox=dict(boxstyle="round", fc="0.9", ec="gray"), ha='center')
+print('\nAverage Score per Year graph:')
 plt.show()
+print('Average score in all editions: {}'.format(round(complete_info_df['Score'].mean(), 3)))
+print('Average medalists score in all editions: {}\n'.format(round(medalists['Score'].mean(), 3)))
 
-# Gráficos de barras com diff entre 1o e ultimo classificado e primeiro e 3 classificado.
+
+# GRAPH 2
+# Gráficos de barras com diff entre 1o e último classificado e 1o e 3o classificado.
 
 first_last = []
 first_third = []
+third_last = []
+
 for year in complete_info_df['Year'].unique():
     max_range, podium_range = calc_diff(complete_info_df, year)
     first_last.append(max_range)
     first_third.append(podium_range)
+    third_last.append(max_range-podium_range)
 
 first_third_range = plt.bar(
     complete_info_df['Year'].unique(), first_third, width=1.8, color='gold')
 first_last_range = plt.bar(
-    complete_info_df['Year'].unique(), first_last, bottom=first_third, width=1.8)
+    complete_info_df['Year'].unique(), third_last, bottom=first_third, width=1.8)
 plt.xticks(complete_info_df['Year'].unique(),
            complete_info_df['Year'].unique())
 plt.legend((first_third_range[0], first_last_range[0]),
-           ('Medalists', 'All Athletes'))
+           ('Medalists', 'All Athletes'), loc = 'upper center')
 plt.ylim(0, 180)
 plt.ylabel('Scores')
 plt.xlabel('Year')
 plt.title('Range of Scores')
+print('\nDifference between first and last place and difference between first and third place in each edition graph:')
 plt.show()
+print('Average difference between first and last place: {}'.format(sum(first_last)/len(first_last)))
+print('Biggest difference between first and last place: {}'.format(max(first_last)))
+print('Lowest difference between first and last place: {}'.format(min(first_last)))
+print('Average difference between first and third place: {}'.format(sum(first_third)/len(first_third)))
+print('Biggest difference between first and third place: {}'.format(max(first_third)))
+print('Lowest difference between first and third place: {}'.format(min(first_third)))
 
+
+# TABLE 2
 # Tabela com tentativas falhadas
 
 tries = {}
@@ -161,13 +175,12 @@ for year in sports_events['year'].unique():
     total_lifts = len(year_df['athlete_id'].unique())
     snatch_lifts = len(year_df[year_df['lift_type']
                                == 'snatch']['athlete_id'].unique())
-    cj_lifts = len(year_df[year_df['lift_type'] ==
-                           'cleanjerk']['athlete_id'].unique())
+    cj_lifts = len(year_df[year_df['lift_type'] == 'cleanjerk']['athlete_id'].unique())
+    
     failed_lifts = year_df['lift_valid'].value_counts()[False]
-    failed_snatch = year_df[year_df['lift_type'] ==
-                            'snatch']['lift_valid'].value_counts()[False]
-    failed_cj = year_df[year_df['lift_type'] ==
-                        'cleanjerk']['lift_valid'].value_counts()[False]
+    failed_snatch = year_df[year_df['lift_type'] == 'snatch']['lift_valid'].value_counts()[False]
+    failed_cj = year_df[year_df['lift_type'] == 'cleanjerk']['lift_valid'].value_counts()[False]
+    
     winner_id = int(position_by_ratio_df[(position_by_ratio_df['Year'] == year) & (
         position_by_ratio_df['Position'] == 1)]['Athlete ID'])
     failed_winner_lifts = len(
@@ -176,6 +189,7 @@ for year in sports_events['year'].unique():
         year_df['athlete_id'] == winner_id) & (year_df['lift_valid'] == False)])
     failed_winner_cj = len(year_df[(year_df['lift_type'] == 'cleanjerk') & (
         year_df['athlete_id'] == winner_id) & (year_df['lift_valid'] == False)])
+    
     tries[year] = [failed_snatch/snatch_lifts, failed_cj/cj_lifts, failed_lifts /
                    total_lifts, failed_winner_snatch, failed_winner_cj, failed_winner_lifts]
 
@@ -183,12 +197,18 @@ failed_lifts_df = pd.DataFrame.from_dict(tries, orient='index',
                                          columns=['Failed Snatch', 'Failed Clean and Jerk', 'Overall Failed', 'Failed Snatch', 'Failed Clean and Jerk', 'Overall Failed'])
 
 failed_lifts_df.columns = pd.MultiIndex.from_tuples(list(zip(
-    ['All Athletes', 'All Athletes', 'All Athletes', 'Winner', 'Winner', 'Winner'], failed_lifts_df.columns)))
+    ['Average All Athletes', 'Average All Athletes', 'Average All Athletes', 'Winner', 'Winner', 'Winner'], failed_lifts_df.columns)))
 
+print("\nAverage invalid lifts per modality in each year and winner's invalid lifts:\n")
 print(failed_lifts_df)
+print('\nAverage failed Snatch lifts: {}'.format(round(failed_lifts_df['Average All Athletes','Failed Snatch'].mean(), 3)))
+print('Average failed Clean and Jerk lifts: {}'.format(round(failed_lifts_df['Average All Athletes','Failed Clean and Jerk'].mean(), 3)))
+print('Average failed lifts: {}'.format(round(failed_lifts_df['Average All Athletes','Overall Failed'].mean(), 3)))
+print('\n{} has the lower average of failed lifts: {}'.format(failed_lifts_df[failed_lifts_df['Average All Athletes','Overall Failed'] == failed_lifts_df['Average All Athletes','Overall Failed'].min()].index[0].astype(int), round(failed_lifts_df['Average All Athletes','Overall Failed'].min(),3)))
+print('{} has the highest average of failed lifts: {}\n'.format(failed_lifts_df[failed_lifts_df['Average All Athletes','Overall Failed'] == failed_lifts_df['Average All Athletes','Overall Failed'].max()].index[0].astype(int), round(failed_lifts_df['Average All Athletes','Overall Failed'].max(),3)))
 
-# fazer prints com médias de cada coluna.
 
+# GRAPH 3
 # Gráfico Média de Score vs. Idade
 
 score_mean = []
@@ -204,7 +224,15 @@ plt.bar(
 plt.xticks(np.arange(complete_info_df['Age'].min(), complete_info_df['Age'].max()+1))
 plt.ylim(100, 280)
 plt.ylabel('Scores')
-complete_info_df['Age'].value_counts().sort_index().plot(secondary_y=True, color = 'blue')
-plt.ylabel('Number of Athletes')
 plt.xlabel('Age')
-plt.title('Scores Mean per Age')
+ages = complete_info_df['Age'].value_counts().sort_index()
+ages = ages.reindex(pd.RangeIndex(ages.index.min(), ages.index.max() + 1)).fillna(0)
+ages.plot(secondary_y=True, color = 'blue')
+plt.ylabel('Number of Athletes')
+plt.ylim(0, ages.max()+1)
+plt.title('Average Scores per Age')
+print('\nAverage Scores per Age graph:')
+print('Highest average score: {}'.format(max(score_mean)))
+print('Most common age of participation is {} with {} participants.'.format(ages[ages==ages.max()].index[0].astype(int), int(ages.max())))
+plt.show()
+
